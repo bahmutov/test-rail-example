e710a377f8ca7939f6834251a215fb9030e94215
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-11 13:21:55 -0400

    add the plugin

--- cypress/integration/spec-b.js
+++ cypress/integration/spec-b.js
@@ -1,13 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Test Rail example', () => {
-  it('works 1', () => {
-    cy.wait(1000)
-  })
-  it('works 2', () => {
-    cy.wait(1000)
-  })
-  it('works 3', () => {
+  it('C112 works 3', () => {
     cy.log('in the last test!')
     cy.wait(1000)
   })
